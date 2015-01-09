9db84e4a8c29df3856b5106817a49132e66d167d
Jaccorot <caozijun007@163.com>
2015-01-09 15:51:22 +0800

    fix the bug:unable to load the local static

--- OnlyTime/apps/main/views.py
+++ OnlyTime/apps/main/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render,render_to_response
 import datetime
 
 def index_page(request):
 	now = datetime.datetime.now()
 	current_time = now.strftime('%Y%m%d')
-	return render_to_response('base.html',{'current_time':current_time})
+	return render(request,'base.html',{'current_time':current_time})
