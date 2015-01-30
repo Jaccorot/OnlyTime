781a02946260b09e13dc54938e6ba511963a8234
ddkangfu <ddkangfu@gmail.com>
2015-01-30 23:19:43 +0800

    added:修改密码功能。

--- OnlyTime/apps/main/urls.py
+++ OnlyTime/apps/main/urls.py
@@ -13,4 +13,6 @@ urlpatterns = patterns('',
     url(r'^logout/', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^relogin/', 'django.contrib.auth.views.logout_then_login', name='relogin'),
     url(r'^register/', views.RegisterView.as_view(), name='register'),
+    url(r'^password_change/$', 'django.contrib.auth.views.password_change', name='password_change'),
+    url(r'^password_change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),    
 )
